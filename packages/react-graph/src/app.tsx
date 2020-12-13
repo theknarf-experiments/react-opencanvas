49a5004b420ceba13a23a11ccb470a1006b30b1c
Frank Lyder Bredland <fl@nyggen.com>
2020-12-13 21:47:33 +0100

    Close button

--- packages/react-graph/src/app.tsx
+++ packages/react-graph/src/app.tsx
@@ -11,29 +11,52 @@ const GenericNode1 : React.FC = () => {
 	</Node>
 }
 
-const GenericNode2 : React.FC = () => {
+const GenericNode2 : React.FC<{
+	onClose?: Function
+}> = ({ onClose }) => {
 	const { Node } = useNode();	
+	const onClick = () => {
+		if(typeof onClose === 'function')
+			onClose();
+	}
 
 	return <Node>
 		<div style={{ padding: '20px'}}>
 		generic node type 2	
+		<button onClick={onClick}>Close</button>
 		</div>
 	</Node>
 }
 
 const App : React.FC = () => {
-	const [num, setNum] = useState(0);
+	const [num, setNum] = useState<{
+		max: number;
+		items: number[];
+	}>({
+		max: 0,
+		items: [],
+	});
 	const onClick = () => {
-		setNum(num + 1);
+		const max = num.max +1
+		setNum({
+			max,
+			items: [ ...num.items, max ],
+		});
 	};
+	const remove = (i : number) => {
+		setNum({
+			max: num.max,
+			items: num.items.filter(itm => itm !== i)
+		});
+	}
 
 	return <div>
 		<Graph width={1200} height={500}>
 			<GenericNode1 />
-			<GenericNode2 />
+			<GenericNode1 />
 			{
-				'.'.repeat(num).split('').map((_, i) => (
-					<GenericNode2 key={i} />
+				num.items.map(i => (
+					<GenericNode2 key={i} onClose={() => remove(i)} />
 				))
 			}
 		</Graph>
