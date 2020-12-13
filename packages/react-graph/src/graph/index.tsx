d8c428767064bb5346aec9bf3a546613d571e759
Frank Lyder Bredland <fl@nyggen.com>
2020-12-13 22:29:36 +0100

    Refactor BackgroundGraph into its own file

--- packages/react-graph/src/graph/index.tsx
+++ packages/react-graph/src/graph/index.tsx
@@ -1,6 +1,7 @@
 import React, { createContext, useContext, useReducer, useRef } from 'react';
 import { useUID } from 'react-uid';
 import useDraggable from './useDraggable';
+import BackgroundGraph from './backgroundgraph';
 
 interface Graph {
 	nodes: any[];
@@ -57,62 +58,6 @@ const graphReducer = (state : Graph, action: Actions) => {
 };
 
 const updatePosition = (uid: Uid, x: Number, y: Number) => ({ type: ActionType.UpdatePosition, uid, x, y });
-
-interface BackgroundGraphProps {
-	vertical: number[];
-	horizontal: number[];
-	width: number;
-	height: number;
-};
-
-/*
- *	Repeats an array a number of times
- *
- *  Example:
- *    repeatArray([1, 2, 3], 2); // Result: [1, 2, 3, 1, 2, 3]
- */
-const repeatArray = (array : any[], times : number) =>
-	'.'.repeat(times).split('').flatMap(() => array);
-
-const BackgroundGraph : React.FC<BackgroundGraphProps> = ({ vertical, horizontal, width, height }) => {
-	const viewBox = `0 0 ${width} ${height}`;
-	const sumVertical   = vertical.reduce((a,b) => a+b, 0);
-	const sumHorizontal = horizontal.reduce((a,b) => a+b, 0);
-
-	// Repeat horizontal and vertical lines until they fill the width and height
-	vertical   = repeatArray(vertical, width / sumVertical + 1)
-	horizontal = repeatArray(horizontal, height / sumHorizontal + 1)
-
-	const verticalPath = vertical.reduce(
-		({x, pathCommand}, moveRightBy) => {
-			const newX = x+moveRightBy;
-			const newLine = `M${newX},0 V${height} `;
-
-			return {
-				x: newX,
-				pathCommand: pathCommand+newLine
-			};
-		},
-		{x: 0, pathCommand: ''}
-	).pathCommand;
-
-	const horizontalPath = horizontal.reduce(
-		({y, pathCommand}, moveDownBy) => {
-			const newY = y+moveDownBy;
-			const newLine = `M0,${newY} H${width} `;
-
-			return {
-				y: newY,
-				pathCommand: pathCommand+newLine
-			};
-		},
-		{y: 0, pathCommand: ''}
-	).pathCommand;
-
-	return <svg xmlns="http://www.w3.org/2000/svg" viewBox={viewBox}>
-		<path stroke='#3c529e' d={verticalPath + horizontalPath} />
-	</svg>;
-};
 
 type Color = string;
 
